bf5f4e4b455960a7d73bb0e366a7302c9e89d5db
Edouard de Laage <edouarddelaage@gmail.com>
2018-04-05 19:00:08 +0200

    rangement du dossier et affichage des images des heros sur la page html

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
+const showHeros = (heros) => {
+            return `
+            <div class="name_heros">
+            <h4 id="prenom"></h4>   
+            <img src="${heros.images.xs}" >
+        </div>
+            `
+        }
+
 fetch('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json')
     .then(response => response.json())
     .then(personne => {
-        console.log(personne)
-        // const alumnisElement = document.getElementById('block_alumnis')
-        // const alumniElements = personne.map(showAlumnis).join(' ')
-        // alumnisElement.innerHTML = alumniElements
 
-        // const nbElement = document.getElementById('nb_alumni')
-        // nbElement.innerHTML = (`
-        //         <p>${personne.length} membres correspondent à votre recherche</p>
-        //     `)
-        // console.log(personne)
+        const testElement = document.getElementById('test')
+        const insert = personne.map(showHeros).join(' ')
+        testElement.innerHTML = insert
     })
