0e3c9e96b007fc7c1ea4fc0add1d6d9548b7d1be
Edouard de Laage <edouarddelaage@gmail.com>
2018-04-06 11:43:00 +0200

    Code nettoyé

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ fetch('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json')
     .then(response => response.json())
     .then(personne => {
 
-        const testElement = document.getElementById('test')
+        const testElement = document.getElementById('memory_board')
         const insert = personne.map(showHeros).slice(0, 10)
         testElement.innerHTML = insert
 
